0d84a38fb2484a0d476e970607d1d6bee57a2f7b
Yannic Staudt <yannic@tipi.build>
2024-08-29 18:16:28 +0200

    :wrench: fixing the example

--- examples/zip_entry_write_set_time.cpp
+++ examples/zip_entry_write_set_time.cpp
@@ -9,7 +9,8 @@ int main() {
     zip_entry_open(zip, "test/test-1.txt");
 
     time_t zipentry_time = 1622026718;
-    zip_entry_write_set_time(zip, TESTDATA1, strlen(TESTDATA1), &zipentry_time);
+    zip_entry_write(zip, TESTDATA1, strlen(TESTDATA1));
+    zip_entry_set_time(zip, zipentry_time);
     zip_entry_close(zip);
     // TODO add api to get zip file info as well and test for that to be consistent
 
