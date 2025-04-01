2a574e9eeadbbe72924064c0358202ffcc8fc430
Lambourl <luc@tipi.build>
2025-04-01 14:20:35 +0200

    Revert "🔧 force to have 755 right"
    This reverts commit 258a186901c23892a600883e4b828e88beea51ad.

--- src/zip.c
+++ src/zip.c
@@ -1295,8 +1295,6 @@ int zip_entry_fwrite(struct zip_t *zip, const char *filename) {
     zip->entry.external_attr |= 0x01;
   }
   zip->entry.external_attr |= (mz_uint32)((file_stat.st_mode & 0xFFFF) << 16);
-  zip->entry.external_attr = (mz_uint32)((0755 & 0xFFFF) << 16);
-
   zip->entry.m_time = file_stat.st_mtime;
 
   if (!(stream = MZ_FOPEN(filename, "rb"))) {
